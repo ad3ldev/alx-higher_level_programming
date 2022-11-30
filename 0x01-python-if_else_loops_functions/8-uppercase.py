c55c2f0c07517fc82b99f455738369dfe5408f01
Adel ALX <a.adel.a.fattah@gmail.com>
2022-11-29 16:30:35 -0800

    more bigs

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(string):
     string_new = ""
     for character in string:
         if ord(character) >= 97 and ord(character) <= 122:
-            string_new += f"{ord(character) - 32}:c"
+            string_new += f"{ord(character) - 32:c}"
         else:
-            string_new += f"{ord(character)}:c"
+            string_new += f"{ord(character):c}"
     print("{:s}".format(string_new))
