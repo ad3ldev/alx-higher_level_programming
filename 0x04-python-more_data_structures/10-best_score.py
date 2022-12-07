5f75cc4227662cd18365dbe5d7c87689f02209e5
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2022-12-07 22:05:48 +0200

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,11 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    best_key = list(a_dictionary.keys())[0]
-    best = a_dictionary[best_key]
-    for key, value in a_dictionary.items():
-        if value > best:
-            best = value
-            best_key = key
-    return (best_key)
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
