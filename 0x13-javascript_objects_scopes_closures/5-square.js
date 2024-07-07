6bbc4512622794f9b0fecf65d6232b7685cbb727
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2024-07-07 03:35:07 +0300

    js 2

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,6 @@
-0x13-javascript_objects_scopes_closures/5-square.js
+#!/usr/bin/node
+const fs = require('fs');
+
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
